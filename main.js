74ddcd79c591bf65ec48fb2c17159b1c3f7048be
Hana Halamova <halamovahana1@gmail.com>
2020-12-21 20:30:30 +0100

    Shorter and easier version of the javascript portion

--- main.js
+++ main.js
@@ -1,35 +1,8 @@
 const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
 menuBtn.addEventListener('click', () => {
-  if (menuOpen === false) {
-    menuBtn.classList.add('open');
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove('open');
-    menuOpen = false;
-  }
-});
-
-const burgerMenu = document.querySelector('.menu-btn');
-burgerMenu.addEventListener('click', displayMenu);
-
-function displayMenu() {
-  let menu = document.querySelector('.nav-links')
-
-  if (window.getComputedStyle(menu, null).display === 'none') {
-    menu.style.display = 'flex';
-  } else {
-    menu.style.display = 'none';
-  }
-}
+  let menu = document.querySelector('.nav-links');
+  menu.classList.toggle('hide-mobile')
+  menuBtn.classList.toggle('open')
+})
 
 
-window.addEventListener('resize', resetmenu);
-
-function resetmenu() {
-  if (window.innerWidth > 768) {
-    document.querySelector('.nav-links').style.display = 'flex';
-  } else {
-    document.querySelector('.nav-links').style.display = 'none';
-  }
-}
