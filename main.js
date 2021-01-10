b779201e54c3ad44eaee25ae89e064defef30376
Hana Halamova <halamovahana1@gmail.com>
2021-01-10 14:16:09 +0100

    Back to top button added

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 // Hamburger menu
 const menuBtn = document.querySelector('.menu-btn');
+
 menuBtn.addEventListener('click', () => {
   let menu = document.querySelector('.nav-links');
-  menu.classList.toggle('hide-mobile')
-  menuBtn.classList.toggle('open')
+  menu.classList.toggle('hide-mobile');
+  menuBtn.classList.toggle('open');
 })
 
 
+// Scroll to top button
+const goToTopButton = document.getElementById('top-button');
+
+const scrollFunction = () => {
+  let y = window.scrollY;
+  if (y > 550) {
+    goToTopButton.className = 'top-link show';
+  } else {
+    goToTopButton.className = 'top-link hide';
+  }
+}
+
+window.addEventListener("scroll", scrollFunction);
